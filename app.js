7f2b5a17378055a99d5e86b459eb538b15687d19
Cecile Dezy <cecile.dezy@gmail.com>
2019-04-30 16:40:10 +0200

    grande collecte

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $('.block-share-container').appendTo('.intro-my-event');
 $('.section-home-projects').prepend('<h3>LES COLLECTES EN COURS</h3>');
 
 // AJOUT TITRE "MERCI DONATEURS" SUR EVENT GRANDE COLLECTE
-$('.event_60 .section-my-event .row .medium-3, .event_55 .section-my-event .row .medium-3').prepend('<h3 id="donators-title">MERCI DU FOND DU COEUR DE VOTRE SOUTIEN</h3>');
+$('.event_60 .section-my-event .row .medium-3, .event_55 .section-my-event .row .medium-3').prepend('<h3 id="donators-title">MERCI DU FOND DU COEUR<br>DE VOTRE SOUTIEN.</h3>');
 
 
 
