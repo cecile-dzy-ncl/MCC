85ea0d311c2d56658c3aba915e17abe640736718
Cecile Dezy <cecile.dezy@gmail.com>
2019-05-06 15:17:24 +0200

    count

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ $(function(){
     var objectif = $('.objectif-amount').html().split('Objectif')[1].split('  ')[0];
     console.log(objectif);
     $('.stats-event-m-2').html('Objectif: <span>' + $('.objectif-amount').html().split('Objectif')[1].split('  ')[0] + '</span>');
+    console.log('split');
 
     // calcul de l'obj
     $('.objectif-enfants').html(Math.floor(Number.parseInt($('.objectif-amount').html().split('Objectif ')[1].split(' €')[0], 10)/12));
